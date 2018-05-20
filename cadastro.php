4b1948be19849d88cd0fc9014637da3164ab361a
SrNery <rodrigonerymm@gmail.com>
2018-05-20 16:37:46 -0300

    commit inicial

--- cadastro.php
+++ cadastro.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -30,19 +30,31 @@
             <h1 id="cad">Cadastro</h1>             
         <div>
             <form action="/action_page.php">
-                <label for="name">Nome Completo</label>
+                <label for="name">Nome Completo:</label>
                 <input type="text" id="name" name="nomeCompleto" placeholder="Seu nome..">
+
+                <label for="cpf">CPF:</label>
+                <input type="text" id="cpf" name="cpf" placeholder="Insira o CPF..">
+
+
+                <label for="endereco">Endereço:</label>
+                <input type="text" id="endereco" name="endereco" placeholder="Seu Endereço..">
 
                 <label for="email">E-mail:</label>
                 <input type="text" id="email" name="email" placeholder="Seu E-mail..">
 
+                <label for="senha">Senha:</label>
+                <input type="password" id="senha" name="senha" value="123456">
+                <img id="olho" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABDUlEQVQ4jd2SvW3DMBBGbwQVKlyo4BGC4FKFS4+TATKCNxAggkeoSpHSRQbwAB7AA7hQoUKFLH6E2qQQHfgHdpo0yQHX8T3exyPR/ytlQ8kOhgV7FvSx9+xglA3lM3DBgh0LPn/onbJhcQ0bv2SHlgVgQa/suFHVkCg7bm5gzB2OyvjlDFdDcoa19etZMN8Qp7oUDPEM2KFV1ZAQO2zPMBERO7Ra4JQNpRa4K4FDS0R0IdneCbQLb4/zh/c7QdH4NL40tPXrovFpjHQr6PJ6yr5hQV80PiUiIm1OKxZ0LICS8TWvpyyOf2DBQQtcXk8Zi3+JcKfNafVsjZ0WfGgJlZZQxZjdwzX+ykf6u/UF0Fwo5Apfcq8AAAAASUVORK5CYII="/><br>
+
                 <label for="pais">País</label>
                 <select id="pais" name="pais">
                 <option value="australia">Australia</option>
+                <option value="usa">Brasil</option>
                 <option value="canada">Canada</option>
-                <option value="usa">USA</option>
+                <option value="usa">USA</option>                
                 </select>            
-                <input type="submit" value="Submit">
+                <input type="submit" value="Cadastrar">
             </form>
             </article>
         </div>
